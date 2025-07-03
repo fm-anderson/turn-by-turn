a6fed2551192475caa68aa311f506db28695fb77
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2025-07-02 23:29:36 -0400

    GA added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
